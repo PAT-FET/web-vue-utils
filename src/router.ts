f9198bb8e95bb0053ac00598be136d84f6f32902
Alison <zhangyinag@126.com>
2019-07-10 10:18:53 +0800

    0.1.6

--- src/router.ts
+++ src/router.ts
@@ -1,17 +1,19 @@
 import Vue from 'vue'
 
 export function redirect (path: string) {
-  if (Vue.prototype.$router) {
-    Vue.prototype.$router.push(path)
-  } else {
-    window.location.hash = `#${path}`
-  }
+  // if (Vue.prototype.$router) {
+  //   Vue.prototype.$router.push(path)
+  // } else {
+  //   window.location.hash = `#${path}`
+  // }
+  window.location.hash = `#${path}`
 }
 
 export function getCurrentPath () {
-  if (Vue.prototype.$route) {
-    return Vue.prototype.$route.path
-  } else {
-    return window.location.hash.substr(1)
-  }
+  // if (Vue.prototype.$route) {
+  //   return Vue.prototype.$route.path
+  // } else {
+  //   return window.location.hash.substr(1)
+  // }
+  return window.location.hash.substr(1)
 }
--- types/router.d.ts
+++ types/router.d.ts
@@ -1,2 +1,2 @@
 export declare function redirect(path: string): void;
-export declare function getCurrentPath(): any;
+export declare function getCurrentPath(): string;
